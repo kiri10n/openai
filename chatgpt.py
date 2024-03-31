31b4fb973b85805de87eb372d233d1de4f43d4c8
kiritohaze <kiritohaze@gmail.com>
2024-03-31 22:31:12 +0900

    ファイルからchargptに入力できるようにした。改行コードをCRLFとした。

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import os
+import datetime
 from dotenv import load_dotenv
 
 # APIキー
@@ -7,18 +8,49 @@ load_dotenv()
 client = OpenAI(
   api_key=os.environ['OPENAI_API_KEY'],  # this is also the default, it can be omitted
 )
-question = """
-"""
 
-client = OpenAI()
+def main():
+    #
+    # settings
+    #
+    input_file = "input/input.txt"
+    model = "gpt-4-0125-preview"
+    # 現在の日付を取得
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    output_file = f"./out/script_{current_time}.txt"
+    #
+    # settings
+    #
 
-# ChatGPTにテキスト生成をリクエスト
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": question}
-    ]
-)
+    try:
+        # 入力ファイルから翻訳前の文字列を取り出す
+        with open(input_file, mode="r", encoding="utf-8") as input_file:
+             input_text = input_file.read() # 全てのテキストを読み出す
 
-# 生成されたテキストを表示
-print(response.choices[0].message.content)
+
+        client = OpenAI()
+
+        # ChatGPTにテキスト生成をリクエスト
+        response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": input_text}
+            ]
+        )
+
+        output_text = response.choices[0].message.content
+
+        # 生成されたテキストを表示
+        print(output_text)
+
+        # 生成されたテキストをファイル出力
+        with open(output_file, mode='w', encoding='utf-8') as output_file:
+            output_file.write(f'input : {input_text}\n')
+            output_file.write(f'output: {output_text}\n')
+    except FileNotFoundError:
+        print(f'エラー: ファイルが見つかりません')
+    except IOError:
+        print(f'エラー: ファイルを書き込む際にエラーが発生しました。')
+
+if __name__ == "__main__":
+    main()
